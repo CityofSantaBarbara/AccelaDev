e1845e077c1102a98d216aa795ee71692ad1db13
chad weiffenbach <24942229+ChadSLS@users.noreply.github.com>
2021-01-24 18:02:09 -0700

    Update ASA;Planning!~!~!~.js
    Updated with new ASI field updates per Danny's request for application list management

--- Scripts/Event/ASA;Planning!~!~!~.js
+++ Scripts/Event/ASA;Planning!~!~!~.js
@@ -13,4 +13,28 @@
 //			10-17-2019		Chad			Original
 // ********************************************************************************************************
 logDebug("Start of ASA:Planning/*/*/*");
-logDebug("End of ASA:Planning/*/*/*");
+var asiFieldandValue = "";
+var lookupValue = null;
+
+if (publicUser) {
+	lookupValue = "publicUser";
+	logDebug("the lookup is:"+lookupValue);
+	asiFieldandValue = "" + lookup("PLN_APPLICATION_LIST_ASA", lookupValue);
+	logDebug("the asiFieldandValue is:"+asiFieldandValue);
+}
+else {
+	lookupValue = "backOffice";
+	logDebug("the lookup is:"+lookupValue);
+	asiFieldandValue = "" + lookup("PLN_APPLICATION_LIST_ASA", lookupValue);
+	logDebug("the asiFieldandValue is:"+asiFieldandValue);
+}
+
+if (asiFieldandValue.indexOf("::") > -1 ) { 
+	var asiFieldValArr = asiFieldandValue.split("::");
+	var asiField = asiFieldValArr[0];
+	var asiVal = asiFieldValArr[1];
+	logDebug("the asi Field is:"+asiField);
+	logDebug("the asi Value is:"+asiVal);
+	editAppSpecific(asiField,asiVal);
+} 
+else logDebug("look up not found!");logDebug("End of ASA:Planning/*/*/*");
