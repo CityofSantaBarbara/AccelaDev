c7849beca9a2eca97bec965ec511e119f5e8e806
chad weiffenbach <24942229+ChadSLS@users.noreply.github.com>
2021-02-09 15:41:07 -0700

    Update DUA;PLANNING!~!~!~.js
    added check if filedate is today.

--- Scripts/Event/DUA;PLANNING!~!~!~.js
+++ Scripts/Event/DUA;PLANNING!~!~!~.js
@@ -2,7 +2,18 @@ var asiFieldandValue = "";
 var lookupValue = null;
 
 if (publicUser) {
-	lookupValue = "publicUser";
+	var fileDate = convertDate(cap.getFileDate()); 
+	fileDate.setHours(0,0,0,0);
+	var localToday = new Date();
+	localToday.setHours(0,0,0,0);  //strip the time out of the date
+	
+	if ( fileDate.toString() == localToday.toString() ) { 
+		lookupValue = "NewAppPublicUser";
+	}
+	else { 
+		lookupValue = "publicUser";
+	}
+	
 	logDebug("the lookup is:"+lookupValue);
 	asiFieldandValue = "" + lookupOnlyActive("PLN_APPLICATION_LIST_DUA", lookupValue);
 	logDebug("the asiFieldandValue is:"+asiFieldandValue);
