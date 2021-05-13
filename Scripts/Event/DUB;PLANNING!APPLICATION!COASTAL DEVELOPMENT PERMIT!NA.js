698406c1b4b78528ada970cc1e3e848fc800cd69
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2021-05-13 10:20:22 -0700

    update

--- Scripts/Event/DUB;PLANNING!APPLICATION!COASTAL DEVELOPMENT PERMIT!NA.js
+++ Scripts/Event/DUB;PLANNING!APPLICATION!COASTAL DEVELOPMENT PERMIT!NA.js
@@ -3,10 +3,10 @@
 //Added by Gray Quarter Ticket #1699
 //Start - Stop Documents from being uploaded thru ACA on CDO records
 
-showMessage = true;
 if(publicUser){
     cancel = true;
-    comment("Uploading documents is not allowed for this record.");
+    showMessage = true;
+    showDebug = false;
+    message = "Uploading documents is not allowed for this record.";
 }
- 
 //END - Stop Documents from being uploaded thru ACA on CDO records 
