4ae8b8759cc97ada30a3b1bb362a4f04cad4fe83
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2023-07-06 10:24:01 -0700

    Update

--- Scripts/Async/ASYNC_ONDEMAND_EVCHARGING_SEND_EMAIL.js
+++ Scripts/Async/ASYNC_ONDEMAND_EVCHARGING_SEND_EMAIL.js
@@ -85,3 +85,75 @@ if (capContactResult.getSuccess()) {
     }
   }
 }
+
+
+function generateReportForASyncEmail(itemCap, reportName, module, parameters) {
+  //returns the report file which can be attached to an email.
+  var vAltId;
+  var user = currentUserID; // Setting the User Name
+  var report = aa.reportManager.getReportInfoModelByName(reportName);
+  var permit;
+  var reportResult;
+  var reportOutput;
+  var vReportName;
+  report = report.getOutput();
+  report.setModule(module);
+  report.setCapId(itemCap);
+  report.setReportParameters(parameters);
+  
+  vAltId = itemCap.getCustomID();
+  report.getEDMSEntityIdModel().setAltId(vAltId);
+  
+  permit = aa.reportManager.hasPermission(reportName, user);
+  if (permit.getOutput().booleanValue()) {
+  reportResult = aa.reportManager.getReportResult(report);
+  if (!reportResult.getSuccess()) {
+  logDebug("System failed get report: " + reportResult.getErrorType() + ":" + reportResult.getErrorMessage());
+  return false;
+  } else {
+  reportOutput = reportResult.getOutput();
+  vReportName = reportOutput.getName();
+  logDebug("Report " + vReportName + " generated for record " + itemCap.getCustomID() + ". " + parameters);
+  return vReportName;
+  }
+  } else {
+  logDebug("Permissions are not set for report " + reportName + ".");
+  return false;
+  }
+  }
+  
+  function generateReport4Save(itemCap, reportName, module, parameters) {
+  //returns the report file which can be attached to an email.
+  var user = currentUserID;   // Setting the User Name
+  var report = aa.reportManager.getReportInfoModelByName(reportName);
+  report = report.getOutput();
+  report.setModule(module);
+  report.setCapId(itemCap.getCustomID());
+  report.setReportParameters(parameters);
+  var edms = new com.accela.v360.reports.proxy.EDMSEntityIdModel();
+  edms.setCapId(itemCap.getID1() + "-" + itemCap.getID2() + "-" + itemCap.getID3());
+  edms.setAltId(itemCap.getCustomID());
+  report.setEDMSEntityIdModel(edms)
+  var permit = aa.reportManager.hasPermission(reportName, user);
+  if (permit.getOutput().booleanValue()) {
+  var reportResult = aa.reportManager.getReportResult(report);
+  if (reportResult.getSuccess()) {
+  reportOutput = reportResult.getOutput();
+  if (reportOutput != null) {
+    var reportFile = aa.reportManager.storeReportToDisk(reportOutput);
+    reportFile = reportFile.getOutput();
+    return reportFile;
+  }
+  else {
+    logDebug("ERROR: No temp file available");
+    return false;
+  }
+  } else {
+  logDebug("System failed get report: " + reportResult.getErrorType() + ":" + reportResult.getErrorMessage());
+  return false;
+  }
+  } else {
+  logDebug("You have no permission.");
+  return false;
+  }
+  }
