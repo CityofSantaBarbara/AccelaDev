e3d9a6d2c51e3312d72c164cc9d8c18c57c4f55c
SBAccela <ITadmin@SantaBarbaraCA.gov>
2023-10-06 11:00:08 -0700

    Update ISB;BUILDING!~!~!~.js
    addComboInspection for Initial Erosion Control

--- Scripts/Event/ISB;BUILDING!~!~!~.js
+++ Scripts/Event/ISB;BUILDING!~!~!~.js
@@ -11,3 +11,21 @@ if(capStatus != "Issued" && capStatus != "Permit Issued"){
 	comment("Inspections may not be scheduled until a permit has been issued!");
 	cancel = true;
 }
+
+function addComboInspection(rInspArr) {
+    for (var x in rInspArr) {
+        var rInspection = rInspArr[x];
+        var rInsp = uObj.getOutput();
+        var rPhone = inspObj.getRequestPhoneNum();
+        var rCom = inspObj.getInspectionComments();
+        var rDate = inspObj.getScheduledDate();
+        var rTime = inspObj.getScheduledTime();
+        aa.inspection.scheduleInspection(capId, rInsp, rDate, rTime, rInspection, rPhone + ' ' + rCom);
+    }
+}
+
+if (inspType.equals('Underground Electric')) {
+// list of the inspection types that are then scheduled.
+    rInspArr = new Array('Initial Erosion Control');
+    addComboInspection(rInspArr);
+}
