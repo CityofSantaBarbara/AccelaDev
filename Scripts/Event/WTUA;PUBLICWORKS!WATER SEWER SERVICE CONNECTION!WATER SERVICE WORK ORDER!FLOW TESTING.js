ac051ce1402ebad9e8bf7f3c31f11edcc8651935
SBAccela <ITadmin@SantaBarbaraCA.gov>
2024-03-20 13:29:46 -0700

    Update WTUA;PUBLICWORKS!WATER SEWER SERVICE CONNECTION!WATER SERVICE WORK ORDER!FLOW TESTING.js

--- Scripts/Event/WTUA;PUBLICWORKS!WATER SEWER SERVICE CONNECTION!WATER SERVICE WORK ORDER!FLOW TESTING.js
+++ Scripts/Event/WTUA;PUBLICWORKS!WATER SEWER SERVICE CONNECTION!WATER SERVICE WORK ORDER!FLOW TESTING.js
@@ -2,28 +2,60 @@
 //Ticket #SR-99583
 //WTUA:PublicWorks/Water Sewer Service Connection/Water Service Work Order/Flow Testing
 
-if (wfTask == "Work Order Issuance" && wfStatus == "Issued") {
-    logDebug("PBW Fire Hydrant Flow Test Work Order Notification");
-//Get Report and Report Parameters
-   
- var fromEmail = lookup("SCRIPT_EMAIL_FROM", "AGENCY_FROM");
-     var toEmail = "nfolman@santabarbaraca.gov";
-     var ccEmail = "nfolman@santabarbaraca.gov"; //blank for now
-     var theURL = "";
-     var emailParameters = aa.util.newHashtable();
+//Script to auto send email notification to Roger and Jesus when the Work Order Issuance task is set to Issued.
 
-     addParameter(emailParameters, "$$RecordID$$", cap.getCapModel().getAltID());
-     //var someaddress = getAddressInALineLocal(capId);
-     addParameter(emailParameters, "$PERMITADDR$", someaddress);
-     //var workDesc = workDescGet(capId);
-     addParameter(emailParameters, "$PERMITWRKDESC$", workDesc);
-     //var wfComment = fTask.getDispositionComment();
-     addParameter(emailParameters, "$TASKCOMMENTS$", wfcomment);
-
-     var emailTemplate = "PBW_WD_FIREHYDRANT_ISSUED";
-     var capId4Email = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
-     var fileNames = [];
+try{
+    if (wfTask == "Work Order Issuance" && wfStatus == "Issued") {
+        logDebug("*****************PBW Fire Hydrant Flow Test Work Order Notification*****************");
+    //Get Report and Report Parameters
     
-     aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, ccEmail, emailTemplate, emailParameters, capId4Email, fileNames);
-     logDebug( ": Sent Fire Hydrant Flow Email template " + emailTemplate + " To Roger and Jesus ");
+        var fromEmail = lookup("SCRIPT_EMAIL_FROM", "AGENCY_FROM");
+        var toEmail = "nfolman@santabarbaraca.gov;";
+        var ccEmail = ""; //blank for now
+        var theURL = "";
+        var emailParameters = aa.util.newHashtable();  
+
+        addParameter(emailParameters, "$$RecordID$$", cap.getCapModel().getAltID());
+
+        var addressResult = aa.address.getAddressByCapId(capId);
+           if (addressResult.getSuccess()) {
+               var addressArray = addressResult.getOutput();
+               if (addressArray.length > 0) {
+                   var address = addressArray[0];
+                   var displayAddress = address.getDisplayAddress();
+                   logDebug("Address: " + displayAddress);
+                   addParameter(emailParameters, "$$Address$$", displayAddress);
+               } else {
+                   logDebug("No addresses found for this Cap ID.");
+               }
+           } else {
+               logDebug("**ERROR: Failed to get addresses " + addressResult.getErrorMessage());
+           }
+
+        var capModel = cap.getCapModel();
+        var workDescModel = capModel.getCapWorkDesModel();
+        var workDescription = workDescModel.getDescription();
+        logDebug("Work Description: " + workDescription);
+        addParameter(emailParameters, "$$WorkDesc$$", workDescription);
+
+        var wfTaskResult = aa.workflow.getTask(capId, wfTask);
+            if (wfTaskResult.getSuccess()) {
+                wfTask = wfTaskResult.getOutput();
+                var wfComment = wfTask.getDispositionComment();
+                logDebug("Workflow Comment: " + wfComment);
+                addParameter(emailParameters, "$$WorkflowComment$$", wfComment);
+            } else {
+                logDebug("**ERROR: Failed to get workflow task " + wfTaskResult.getErrorMessage());
+            }
+
+        var emailTemplate = "PBW_WD_FIREHYDRANT_ISSUED";
+        var capId4Email = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
+        var fileNames = [];
+
+        aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, ccEmail, emailTemplate, emailParameters, capId4Email, fileNames);
+        logDebug( ": Sent Fire Hydrant Flow Email template " + emailTemplate + " To Roger and Jesus ");
+    }
+} catch (err) { 
+    logDebug("A JavaScript Error occurred: WTUA:PublicWorks/Water Sewer Service Connection/Water Service Work Order/Flow Testing: " + err.message); 
+    logDebug(err.stack);
 }
